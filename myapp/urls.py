0c786845d314ee4db2175dae8e32d47bf9fdf459
MrGroot01 <kirand09876@gmail.com>
2026-04-07 13:51:51 +0530

    homepage changes

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import register_user, login_user
+from .views import register_user, login_user, google_login, stats
 
 urlpatterns = [
     path("register/", register_user),
     path("login/", login_user),
+    path("google-login/", google_login),   # ✅ NEW
+    path("stats/", stats),                 # ✅ OPTIONAL
 ]
